62b2b5417d248044c7ffe8ecaceffff251c924dc
lunchdotbox <fsapora@outlook.com>
2026-08-16 17:17:45 -0400

    stuff

---
+++ src/main/java/gay/lunch/createbigmail/munitions/big_cannon/mail_shot/PostboxDetector.java
@@ -0,0 +1,9 @@
+package gay.lunch.createbigmail.munitions.big_cannon.mail_shot;
+
+import com.simibubi.create.content.logistics.packagePort.postbox.PostboxBlockEntity;
+
+import java.util.List;
+
+public class PostboxDetector {
+    private static List<PostboxBlockEntity>
+}
